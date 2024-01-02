9ea8ff7d020f056f0854c683934260347c3ae135
prk-vanga <praveen-dev@hotmail.com>
2024-01-02 23:58:51 +0530

    DB setup done, Django management custom command has been set and tested working properly"

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,7 +2,7 @@
 Django Command to wait for the database to be available
 """
 import time
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error  # noqa
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
